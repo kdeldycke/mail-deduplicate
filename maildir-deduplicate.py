fe1877b47dfb32e77c05aa3a8fd4225d99ae901c
Kevin Deldycke <kevin@deldycke.com>
2011-04-28 00:30:02 +0200

    Add credits to Adam Spiers.

--- maildir-deduplicate.py
+++ maildir-deduplicate.py
@@ -2,7 +2,8 @@
 
 ##############################################################################
 #
-# Copyright (C) 2010 Kevin Deldycke <kevin@deldycke.com>
+# Copyright (C) 2010-2011 Kevin Deldycke <kevin@deldycke.com>
+#                         Adam Spiers <adam@spiers.net>
 #
 # This program is Free Software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
