328a9959b2d016f56d064e2a371c9acaa9de6f82
Kevin Deldycke <kdeldycke@scaleway.com>
2016-08-09 12:24:15 +0200

    Add Python 2 and Python 3 flags.

--- maildir_deduplicate/__init__.py
+++ maildir_deduplicate/__init__.py
@@ -20,8 +20,14 @@
 """ Expose package-wide elements. """
 
 import logging
+import sys
+
 
 __version__ = '1.3.0'
+
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
 
 
 logger = logging.getLogger(__name__)
