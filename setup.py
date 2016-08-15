a65ffd13dd1d3cf391538229feff51dbe3bd8b0d
Kevin Deldycke <kevin@deldycke.com>
2016-08-15 16:24:28 +0200

    Add common headers.

--- setup.py
+++ setup.py
@@ -17,6 +17,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals
+)
+
 import codecs
 import os
 import re
