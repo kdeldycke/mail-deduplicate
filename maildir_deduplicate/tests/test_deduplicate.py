75549ec292787fac0dd33fc2214b16baf379f0d0
Kevin Deldycke <kevin@deldycke.com>
2016-09-27 01:07:15 +0200

    Add the usual future suspects.

--- maildir_deduplicate/tests/test_deduplicate.py
+++ maildir_deduplicate/tests/test_deduplicate.py
@@ -18,7 +18,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-from __future__ import absolute_import, division, print_function
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals
+)
 
 from os import makedirs, path
 from textwrap import dedent
