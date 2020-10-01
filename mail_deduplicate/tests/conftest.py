0723872c335ff7648600fe1a39c88664f6d823d8
Kevin Deldycke <kevin@deldycke.com>
2020-10-01 22:44:29 +0200

    Move test utilities in conftest.

--- mail_deduplicate/tests/conftest.py
+++ mail_deduplicate/tests/conftest.py
@@ -17,8 +17,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-from textwrap import indent
+from email.utils import formatdate as maildate
+from mailbox import Mailbox, Maildir, Message, mbox
+from textwrap import dedent, indent
+from uuid import uuid4
 
+import arrow
 import click
 import pytest
 from boltons.iterutils import flatten, same
@@ -26,8 +30,8 @@ from boltons.strutils import strip_ansi
 from boltons.tbutils import ExceptionInfo
 from click.testing import CliRunner
 
-from ..cli import mdedup
 from .. import CLI_NAME
+from ..cli import mdedup
 
 """ Fixtures, configuration and helpers for tests. """
 
@@ -73,3 +77,127 @@ def invoke(runner):
         return result
 
     return _run
+
+
+class MailFactory:
+
+    """Create fake mail messages to serve as unittest fixtures.
+
+    Help production of either random, customized or deterministic mail message.
+    """
+
+    def __init__(self, **custom_fields):
+        """Init the mail with custom fields.
+
+        You can bypass data normalization by passing the pre-formated date
+        string with ``date_rfc2822`` custom field instead of ``date``.
+        """
+        # Defaults fields values.
+        self.fields = {
+            "body": "Да, они летят.",
+            "date": arrow.utcnow(),
+            "date_rfc2822": None,
+        }
+
+        # Check all custom fields are recognized and supported.
+        assert set(custom_fields).issubset(self.fields)
+
+        # Parse dates and normalize to Arrow instance.
+        if "date" in custom_fields:
+            custom_fields["date"] = arrow.get(custom_fields["date"])
+
+        # Update default values with custom ones.
+        self.fields.update(custom_fields)
+
+        # Derive RFC-2822 date from arrow object if not set.
+        if not self.fields.get("date_rfc2822"):
+            self.fields["date_rfc2822"] = maildate(self.fields["date"].float_timestamp)
+
+    def render(self):
+        """ Returns the full, rendered content of the mail. """
+        return dedent(
+            """\
+            Return-path: <none@nohost.com>
+            Envelope-to: me@host.com
+            Delivery-date: {date_rfc2822}
+            Received: from [11.11.11.11] (helo=nope.com)
+            \tby host.com with esmtp (Exim 4.80)
+            \t(envelope-from <noone@nohost.com>)
+            \tid 1CX8OJ-0014c9-Ii
+            \tfor me@host.com; {date_rfc2822}
+            Date: {date_rfc2822}
+            From: foo@bar.com
+            Message-Id: <201111231111.abcdef101@mail.nohost.com>
+            To: baz
+            Subject: A duplicate mail
+            Mime-Version: 1.0
+            Content-Length: 60
+            Content-Type: text/plain; charset="utf-8"
+            Content-Transfer-Encoding: 8bit
+            {body}""".format(
+                **self.fields
+            )
+        ).encode("utf-8")
+
+    def as_message(self):
+        """Returns the mail as an instance of ``mailbox.Message``."""
+        return Message(self.render())
+
+
+@pytest.fixture
+def make_box(tmp_path):
+    """A generic fixture to produce a temporary box of mails.
+
+    The mail container can be created in any format supported by Python standard
+    library, by the way of the ``box_type`` parameter. Supported values: only
+    ``Maildir`` and ``mbox`` for the moment.
+    """
+
+    def _make_mailbox(box_type, mails):
+        """Create a fake maildir and populate it with mails."""
+        # Check parameters.
+        assert box_type in (Maildir, mbox)
+        assert issubclass(box_type, Mailbox)
+        assert same(map(type, mails), MailFactory)
+
+        # Create the container under a random name and put all provided mails there.
+        box = box_type(tmp_path.joinpath(uuid4().hex), create=True)
+        box.lock()
+        for fake_mail in mails:
+            box.add(fake_mail.render())
+
+        box.close()
+        return box._path
+
+    return _make_mailbox
+
+
+def check_box(box_path, box_type, kept=None, deleted=None):
+    """Check the content of a mail box (in any of maildir of mbox format).
+
+    Does not use ``set()`` types internally to avoid silent deduplication.
+    Translates all mails provided to ``mailbox.Message`` instances to provide
+    fair comparison in a normalized space.
+    """
+    # Check provided parameters.
+    assert isinstance(box_path, str)
+    assert box_type in (Maildir, mbox)
+    for mail_list in (kept, deleted):
+        assert not isinstance(mail_list, set)
+        if mail_list is None:
+            mail_list = []
+        if mail_list:
+            assert same(map(type, mail_list), MailFactory)
+
+    # Compares the content of the box.
+    box = box_type(box_path, create=False)
+
+
+    # TODO: use a COunter to count occurences
+
+    assert len(box) == len(kept)
+    mails_found = sorted([str(m) for m in box])
+    assert sorted([str(m.as_message()) for m in kept]) == mails_found
+    for mail in deleted:
+        assert str(mail.as_message()) not in mails_found
+    box.close()
--- mail_deduplicate/tests/test_deduplicate.py
+++ mail_deduplicate/tests/test_deduplicate.py
@@ -17,145 +17,21 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-from email.utils import formatdate as maildate
-from textwrap import dedent
-from uuid import uuid4
+from mailbox import Maildir, mbox
 
-from mailbox import Mailbox, Message, Maildir, mbox
-
-import pytest
 import arrow
-from boltons.iterutils import same
 
+from .conftest import MailFactory, check_box
 from .. import STRATEGIES
 from ..deduplicate import DuplicateSet
 
 
-class MailFactory:
-
-    """Create fake mail messages to serve as unittest fixtures.
-
-    Help production of either random, customized or deterministic mail message.
-    """
-
-    def __init__(self, **custom_fields):
-        """Init the mail with custom fields.
-
-        You can bypass data normalization by passing the pre-formated date
-        string with ``date_rfc2822`` custom field instead of ``date``.
-        """
-        # Defaults fields values.
-        self.fields = {
-            "body": "Да, они летят.",
-            "date": arrow.utcnow(),
-            "date_rfc2822": None,
-        }
-
-        # Check all custom fields are recognized and supported.
-        assert set(custom_fields).issubset(self.fields)
-
-        # Parse dates and normalize to Arrow instance.
-        if "date" in custom_fields:
-            custom_fields["date"] = arrow.get(custom_fields["date"])
-
-        # Update default values with custom ones.
-        self.fields.update(custom_fields)
-
-        # Derive RFC-2822 date from arrow object if not set.
-        if not self.fields.get("date_rfc2822"):
-            self.fields["date_rfc2822"] = maildate(self.fields["date"].float_timestamp)
-
-    def render(self):
-        """ Returns the full, rendered content of the mail. """
-        return dedent(
-            """\
-            Return-path: <none@nohost.com>
-            Envelope-to: me@host.com
-            Delivery-date: {date_rfc2822}
-            Received: from [11.11.11.11] (helo=nope.com)
-            \tby host.com with esmtp (Exim 4.80)
-            \t(envelope-from <noone@nohost.com>)
-            \tid 1CX8OJ-0014c9-Ii
-            \tfor me@host.com; {date_rfc2822}
-            Date: {date_rfc2822}
-            From: foo@bar.com
-            Message-Id: <201111231111.abcdef101@mail.nohost.com>
-            To: baz
-            Subject: A duplicate mail
-            Mime-Version: 1.0
-            Content-Length: 60
-            Content-Type: text/plain; charset="utf-8"
-            Content-Transfer-Encoding: 8bit
-            {body}""".format(
-                **self.fields
-            )
-        ).encode("utf-8")
-
-    def as_message(self):
-        """Returns the mail as an instance of ``mailbox.Message``."""
-        return Message(self.render())
-
-    def save(self, filepath):
-        """ Save the mail to the filesystem. """
-        with open(filepath, "wb") as mail_file:
-            mail_file.write(self.render())
-        # TODO: find a way to set ctime here so we can test for time-based
-        # deduplication strategies.
-
-
-@pytest.fixture
-def make_box(tmp_path):
-    """A generic fixture to produce a temporary box of mails.
-
-    The mail container can be created in any format supported by Python standard
-    library, by the way of the ``box_type`` parameter. Supported values: only
-    ``Maildir`` and ``mbox`` for the moment.
-    """
-
-    def _make_mailbox(box_type, mails):
-        """Create a fake maildir and populate it with mails."""
-        # Check parameters.
-        assert box_type in (Maildir, mbox)
-        assert issubclass(box_type, Mailbox)
-        assert same(map(type, mails), MailFactory)
-
-        # Create the container under a random name and put all provided mails there.
-        box = box_type(tmp_path.joinpath(uuid4().hex), create=True)
-        box.lock()
-        for fake_mail in mails:
-            box.add(fake_mail.render())
-
-        box.close()
-        return box._path
-
-    return _make_mailbox
-
-
-def check_box(box_path, box_type, kept=None, deleted=None):
-    """Check the content of a mail box (in any of maildir of mbox format).
-
-    Does not use ``set()`` types internally to avoid silent deduplication.
-    Translates all mails provided to ``mailbox.Message`` instances to provide
-    fair comparison in a normalized space.
-    """
-    # Check provided parameters.
-    assert isinstance(box_path, str)
-    assert box_type in (Maildir, mbox)
-    for mail_list in (kept, deleted):
-        assert not isinstance(mail_list, set)
-        if mail_list is None:
-            mail_list = []
-        if mail_list:
-            assert same(map(type, mail_list), MailFactory)
-
-    # Compares the content of the box.
-    box = box_type(box_path, create=False)
-    assert len(box) == len(kept)
-    mails_found = sorted([str(m) for m in box])
-    assert sorted([str(m.as_message()) for m in kept]) == mails_found
-    for mail in deleted:
-        assert str(mail.as_message()) not in mails_found
-    box.close()
+def test_strategy_definitions():
+    """ Test deduplication strategy definitions. """
+    for strategy_id in STRATEGIES:
+        method_id = strategy_id.replace("-", "_")
+        assert hasattr(DuplicateSet, method_id)
+        assert callable(getattr(DuplicateSet, method_id))
 
 
 # Collections of pre-defined fixtures to use in the deduplication tests below.
@@ -168,13 +44,6 @@ biggest_mail = MailFactory(
     body="Hello I am a duplicate mail. With annoying ćĥäŖş. +++++++++"
 )
 
-
-def test_strategy_definitions():
-    """ Test deduplication strategy definitions. """
-    for strategy_id in STRATEGIES:
-        method_id = strategy_id.replace("-", "_")
-        assert hasattr(DuplicateSet, method_id)
-        assert callable(getattr(DuplicateSet, method_id))
 
 
 def test_maildir_smaller_strategy_dry_run(invoke, make_box):
