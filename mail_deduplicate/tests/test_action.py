6c79c4647d40eb903c3a16bc6b9794c98d417746
Kevin Deldycke <kevin@deldycke.com>
2021-09-16 13:49:34 +0400

    Add simple action definition test.

---
+++ mail_deduplicate/tests/test_action.py
@@ -0,0 +1,29 @@
+# Copyright Kevin Deldycke <kevin@deldycke.com> and contributors.
+# All Rights Reserved.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+from string import ascii_lowercase
+
+from ..action import ACTIONS
+
+
+def test_action_definitions():
+    """ Test duplicate action definitions. """
+    for action_id, method in ACTIONS.items():
+        # All actions are lower cases strings, with dashes.
+        assert isinstance(action_id, str)
+        assert set(action_id).issubset(ascii_lowercase + "-")
+        assert callable(method)
